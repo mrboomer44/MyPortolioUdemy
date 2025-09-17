0b35e82a3f9b3b96d6a99e886a9433fc9c01f82c
mrboomer44 <yns.guldas44@gmail.com>
2025-09-17 19:33:16 +0300

    View Component Bitti

--- MyPortolioUdemy/Dal/Entities/Contact.cs
+++ MyPortolioUdemy/Dal/Entities/Contact.cs
@@ -2,7 +2,7 @@
 {
     public class Contact
     {
-        public int ContacyId { get; set; }
+        public int ContactId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Phone1 { get; set; }
---
+++ MyPortolioUdemy/ViewComponents/_ContactComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortolioUdemy.ViewComponents
+{
+    public class _ContactComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
+
